4817514be5044ab3d19e9ff5c48a10e3c9e0ae5a
unknown <sa.munozr20@gmail.com>
2022-02-03 11:44:12 -0500

    2nd JSII commit

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -42,6 +42,15 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if(idioma === 'aleman') {
+    return 'Guten Tag!';
+  } else if (idioma === 'mandarin') {
+    return 'Ni Hao!';
+  } else if (idioma === 'ingles') {
+    return 'Hello!';
+  } else {
+    return 'Hola!';
+  }
 }
 
 function colors(color) {
@@ -52,18 +61,34 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  switch(color) {
+    case "blue":
+      return "This is blue";
+    case "red":
+      return "This is red";
+    case "green":
+      return "This is green"
+    case "orange":
+      return "This is orange"
+    default:
+      return "Color not found";
+  } 
+  
 }
-
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  return (numero===10 || numero===5)
 }
+
+
 
 function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código:
+  return (numero<50 && numero>20)
 }
 
 function esEntero(numero) {
